5e63ac05012f6870e98bb2410bfae5d54cdfeb42
Martin Landalv <martin@landalv.se>
2012-08-31 22:10:37 +0200

    Moved codemirror code from link to compile; fixes error when the attribute is applied to multiple textareas.

--- src/codemirror.js
+++ src/codemirror.js
@@ -1,17 +1,17 @@
 ﻿'use strict';
 
-angular.module('codemirror', []).directive('codemirror', function () {
+angular.module('codemirror', []).directive('codemirror', ['$parse', function ($parse) {
     return {
         restrict: 'A',
-        link: function (scope, element, attrs) {
+        compile: function (element, attrs, transclude) {
             // todo: sync value to model
 
             if (!element.is('textarea')) {
                 throw new Error('Codemirror can only be applied on textarea elements.');
             }
 
-            var options = scope.$eval(attrs.cmOptions) || {},
+            var options = $parse(attrs.cmOptions)(this) || {},
                 codemirror = CodeMirror.fromTextArea(element[0], options);
         }
     };
-});
+}]);
